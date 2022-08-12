c2592a482033ed06c9a27f55202e6251e1203c9d
Palam44 <dpalam44@gmail.com>
2022-08-12 08:40:46 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+
+import org.apache.commons.beanutils.BeanComparator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -5,8 +7,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloworld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorld == helloworld2);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1 == cat2);
+
     }
 }
